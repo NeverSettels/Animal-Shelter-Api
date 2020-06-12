b44b2d2d7523a404639d5d868bfe52dabc56f2d8
Eric Settels <settels.eric@gmail.com>
2020-06-12 13:13:35 -0700

    Add vasic validators

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -5,9 +5,13 @@ namespace AnimalApi.Models
   public class Animal
   {
     public int AnimalId { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string Species { get; set; }
+    [Required]
     public string Gender { get; set; }
+    [Required]
     public int Age { get; set; }
     public bool SpecialMedicalNeeds { get; set; }
     public string PicUrl { get; set; }
